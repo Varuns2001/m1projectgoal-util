b89e3ff69073bc60c6b7252b8b98d0cf81e8f455
Varuns2001 <99092710+Varuns2001@users.noreply.github.com>
2022-02-28 23:40:13 -0800

    Update Dice.h

--- 3_IMPLEMENTATION/inc/Dice.h
+++ 3_IMPLEMENTATION/inc/Dice.h
@@ -1,1 +1,15 @@
-
+#ifndef DICE_H<br>
+#define DICE_H<br>
+/**
+ * @brief Function to find the output of a dice roll
+ * @return int result of dice_output operation
+ * 
+ */
+int dice_output();<br>
+/**
+ * @brief Function to check if output of dice is 6
+ * @param[in] diceout Number whose value is to be checked for 6
+ * @return int result after checking
+ */
+char *diceout6();<br>
+#endif/DICE_H/<br>
